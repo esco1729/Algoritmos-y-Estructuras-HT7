d1b3d737854a9fbb98366d343a32282ec6aebd83
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-03-28 16:37:39 -0600

    Setters and getters

--- src/ComparableAssociation.java
+++ src/ComparableAssociation.java
@@ -1,4 +1,40 @@
 
 public class ComparableAssociation<K, V> {
+	String spanish;
+	String english;
+	String french;
+	
+	public String getSpanish() {
+		return spanish;
+	}
+	
+	public void setSpanish(String spanish) {
+		this.spanish = spanish;
+	}
+	
+	public String getEnglish() {
+		return english;
+		
+	}
+	
+	public void setEnglish() {
+		this.english = english;
+	}
+	
+	public String getFrench() {
+		return french;
+	}
+	
+	public void  setFrench() {
+		this.french = french;
+	}
+	
+	
+	public String toString1() {
+		return getEnglish();
+	}
 
+	public String toString2() {
+		return getFrench();
+	}
 }
