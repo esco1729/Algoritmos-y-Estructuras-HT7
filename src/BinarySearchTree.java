8fca692d4c31cfe98f45688be159c81d0c4bd6fc
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-03-28 08:55:30 -0600

    Update BinarySearchTree.java

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,20 @@
+protected BinaryTree<E> parent;
+protected BinaryTree<E> left, right;
 
 public class BinarySearchTree<E> {
 
+	public BinaryTree() {
+		val = null;
+		parent = null; 
+		left = right = this;
+	}
+	
+	public BinaryTree(E value) {
+		val = value;
+		right = left = new BinaryTree<E>();
+		setLeft(left);
+		setRight(right);
+	}
+	
+	
 }
